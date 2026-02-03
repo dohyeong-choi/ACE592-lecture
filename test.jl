a1c765a3bcc3330200e390b299c952710dc011d3
dohyeong-choi <dc65@illinois.edu>
2026-02-03 15:22:14 -0600

    Update test.jl

--- test.jl
+++ test.jl
@@ -1,9 +1,11 @@
 x = (1e-20 + 1) - 1;
 y = (1e-20) + (1 - 1);
-x == y
+x == y # they are different
 
 println("Machine epsilon ϵ is $(eps(Float64))")
 println("Is 1 + ϵ/2 > 1? $(1.0 + eps(Float64)/2 > 1.0)")
 
 println("The smallest representable number larger than 1.0 is $(nextfloat(1.0))")
 println("The largest representable number smaller than 1.0 is $(prevfloat(1.0))")
+
+
